4ee834b8db0da6a92436a49761c4377cbd5c025b
sophie <sophie@sophies-mac-pro.local>
2015-11-11 15:50:11 +0900

    echo server and client. server is not working.

--- src/sophie/Client.java
+++ src/sophie/Client.java
@@ -4,6 +4,7 @@ import javax.swing.*;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.Socket;
 import java.util.Scanner;
 
@@ -11,24 +12,87 @@ import java.util.Scanner;
  * Created by sophie on 2015. 11. 10..
  */
 public class Client {
+    private Socket socket = null;
+    BufferedReader input = null;
+    PrintWriter output = null;
+    private boolean done = false;
     private static final int DEFAULT_PORT = 9000;
 
-    public static void main(String[] args) throws IOException {
+    public void run (int port) {
         //  server ip address input
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter IP Address of a machine that is running the date service on port " + DEFAULT_PORT + ":");
+        System.out.print("Enter IP Address of a machine that is running the date service on port " + port + ":");
         String serverAddress = scanner.nextLine();
 
         //connection
-        Socket socket = new Socket(serverAddress, DEFAULT_PORT);
+        connect(serverAddress, port);
 
-        //message read
-        BufferedReader input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-        String answer = input.readLine();
-        System.out.println(answer);
-
-
-        socket.close();
-        System.exit(0); // 이건 왜하는거지? 그냥 끝내면 안되나? main 함수의 리턴 타입이 void니까 종료했을 때 값을 반환하려면 System.exit 을 사용한다. 보통 정상종료 했을 때 0을 반환, 에러 있을 때 1을 반환한다고 함.
+        prepareReaderAndWriter();
+        String message = null;
+        while(!done) {
+            System.out.print("message: ");
+            message = scanner.nextLine();
+            messageWrite(message);
+            messageRead();
+            if(message.equals(".bye")) {
+                done = true;
+            }
+        }
+        closeResource();
+        System.exit(0);
     }
+
+    private void connect(String serverAddress, int port) {
+        try {
+            System.out.println("Welcome. I'm s client.");
+            socket = new Socket(serverAddress, DEFAULT_PORT);
+
+        } catch (IOException ioe) {
+            System.out.println(ioe); // TODO. #1.
+            System.exit(1);
+        }
+    }
+
+    private void prepareReaderAndWriter() {
+        //TODO. reader, writer 관련 메소드를 하나의 클래스로 빼자. helper class
+        try {
+            input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            output = new PrintWriter(socket.getOutputStream(), true);
+        } catch (IOException ioe) {
+            System.out.println(ioe); //연결이 끊겼거나 등등 inputStream을 가져올 수없을 때
+            System.exit(1);
+        }
+    }
+
+    private void closeResource() {
+        try {
+            input.close();
+            output.close();
+            socket.close();
+        } catch (IOException ioe) {
+            System.out.println(ioe); // TODO. #1
+            System.exit(1);
+        }
+    }
+
+    private String messageRead() {
+        try {
+            return input.readLine();
+        } catch (IOException ioe) {
+            System.out.println(ioe); //연결이 끊겼거나 등등 inputStream을 가져올 수없을 때
+            done = true;
+            return null;
+        }
+    }
+
+    private void messageWrite(String message) {
+        output.print(message); //TODO. println안해도 되겠지? message에 /n 이 속해있지 않을까?
+    }
+
+    public static void main(String[] args) throws IOException {
+        Client client = new Client();
+        client.run(DEFAULT_PORT); //나중에 PORT 번호를 받을 수 있도록 하기 위해
+    }
+
+
 }
--- src/sophie/Server.java
+++ src/sophie/Server.java
@@ -1,32 +1,109 @@
 package sophie;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.Date;
-import java.util.logging.Logger;
 
 /**
  * Created by sophie on 2015. 11. 10..
+ * 참고 사이트 http://pirate.shu.edu/~wachsmut/Teaching/CSAS2214/Virtual/Lectures/chat-client-server.html 보면서 이해해보기
+ * step1
  */
 public class Server {
+    private ServerSocket listener = null;
+    private Socket socket = null;
+    BufferedReader input = null;
+    PrintWriter output = null;
+    private boolean done = false;
     private static final int DEFAULT_PORT = 9000;
 
-    public static void main(String[] args) throws IOException {
-        ServerSocket listener = new ServerSocket(DEFAULT_PORT);
-        System.out.println("Welcome. I am a Server!");
-        Socket socket;
-
-        while ((socket = listener.accept()) != null) {
-            try {
-                PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
-                out.println(new Date().toString());
-            } finally {
-                socket.close();
-            }
+    public void run(int port) {
+        openListener(port);
+        acceptClient();
+        prepareReaderAndWriter();
+        while (!done) {
+           echo();
         }
+        closeResource();
+        System.exit(0);
+    }
 
-        listener.close();
+    private void openListener(int port) {
+        try {
+            System.out.println("Welcome. I'm s server.");
+            listener = new ServerSocket(port);
+            System.out.println("Listener started: " + listener);
+        } catch (IOException ioe) {
+            System.out.println(ioe); // TODO. #1. 에러 일 때 print하는 게 따로 있으려나? 찾아보자.
+            System.exit(1);
+        }
+    }
+
+    private void acceptClient() {
+        try {
+            System.out.println("waiting for a client...");
+            socket = listener.accept();
+            System.out.println("Client accepted: " + socket);
+        } catch (IOException ioe) {
+            System.out.println(ioe); // TODO. #1
+            // TODO #2 listener.close(); 여기서 닫아야 할 거 같은데 그럼 또 try catch를 써야 함 ㅠㅠ 고민해보자.
+            System.exit(1);
+        }
+    }
+
+    private void closeResource() {
+        try {
+            input.close();
+            output.close();
+            socket.close();
+            listener.close();
+        } catch (IOException ioe) {
+            System.out.println(ioe); // TODO. #1
+            System.exit(1);
+        }
+    }
+
+    private void echo() {
+        String message = messageRead();
+        System.out.println(message);
+        messageWrite(message);
+
+        if(message.equals(".bye")){
+            done = true;
+        }
+    }
+
+    private void prepareReaderAndWriter() {
+        try {
+            input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            output = new PrintWriter(socket.getOutputStream(), true);
+        } catch (IOException ioe) {
+            System.out.println(ioe); //연결이 끊겼거나 등등 inputStream을 가져올 수없을 때
+            System.exit(1);
+        }
+    }
+
+    private String messageRead() {
+        try {
+            return input.readLine();
+        } catch (IOException ioe) {
+            System.out.println(ioe); //연결이 끊겼거나 등등 inputStream을 가져올 수없을 때
+            done = true;
+            return null;
+        }
+    }
+
+    private void messageWrite(String message) {
+        output.print(message); //TODO. println안해도 되겠지? message에 /n 이 속해있지 않을까?
+    }
+
+
+    /* main method */
+    public static void main(String args[]) {
+        Server server = new Server();
+        server.run(DEFAULT_PORT); //나중에 PORT 번호를 받을 수 있도록 하기 위해
     }
 }
