f6a773d5243dff0c852a99e33f27ecad10064afd
Kim Dahye <dahye.kim@nhnnext.org>
2015-12-10 17:21:20 +0900

    nio simple server

---
+++ src/sophie/utils/CastUtils.java
@@ -0,0 +1,14 @@
+package sophie.utils;
+
+/**
+ * Created by sophie on 2015. 12. 10..
+ */
+public class CastUtils {
+    public static int byteArrayToInt(byte[] b)
+    {
+        return   b[3] & 0xFF |
+                (b[2] & 0xFF) << 8 |
+                (b[1] & 0xFF) << 16 |
+                (b[0] & 0xFF) << 24;
+    }
+}
